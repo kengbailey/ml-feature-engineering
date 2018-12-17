9c6c10c4f79997a98002d71d0b86cde9a244ce57
Ken Bailey <kgbailey92@gmail.com>
2018-12-17 07:30:41 -0500

    updated with to be more pythonic

--- reverse_geocode.py
+++ reverse_geocode.py
@@ -16,15 +16,15 @@ class FetchZipCodes(threading.Thread):
     Thread fetching zipcodes, given dict of lat/long values
     """
 
-    def __init__(self, zipDict, outDict, tLock):
+    def __init__(self, in_dict, out_dict, tLock):
         threading.Thread.__init__(self)
-        self.zipDict = zipDict
+        self.in_dict = in_dict
         self.gmaps = googlemaps.Client(gmaps_api_key)     
-        self.outDict = outDict   
+        self.out_dict = out_dict   
         self.lock = tLock
 
     def run(self):
-        for key, val in self.zipDict.items():
+        for key, val in self.in_dict.items():
             if val[0] != 0 and val[1] != 0:
                 try:
                     result = self.gmaps.reverse_geocode((str(val[0]), str(val[1])))
@@ -39,22 +39,22 @@ class FetchZipCodes(threading.Thread):
                         break
 
             with self.lock:
-                self.outDict[key] = zipcode
+                self.out_dict[key] = zipcode
 
             zipcode = '00000'
 
 if __name__ == "__main__":
     
-    inList = {'abc':['40.711303', '-74.016048'], 
+    in_dict = {'abc':['40.711303', '-74.016048'], 
                 '123':['40.76127', '-73.982738'], 
                 'xyz':['35.89250', '-86.31699'], 
                 '901':['40.733873', '-73.980658']}
-    outDict = {}
+    out_dict = {}
 
     tLock = threading.Lock()
-    t = FetchZipCodes(inList, outDict, tLock)
+    t = FetchZipCodes(in_dict, out_dict, tLock)
     t.start()
     t.join()
 
-    for k,v in outDict.items():
+    for k,v in out_dict.items():
         print(k,v)
